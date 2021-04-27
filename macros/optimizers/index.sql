b874784a4537ff66c05255a946a070b4b673ecc0
Ken Geis <geis.ken@gmail.com>
2021-04-27 09:20:33 -0700

    remove schema qualifier from index name
    From PostgreSQL docs: "No schema name can be included here; the index is always created in the same schema as its parent table."

--- macros/optimizers/index.sql
+++ macros/optimizers/index.sql
@@ -1,5 +1,5 @@
 {% macro index(this, column) %}
 
-create index if not exists "{{ this.schema }}.{{ this.name }}__index_on_{{ column }}" on {{ this.schema }}.{{ this }} ("{{ column }}")
+create index if not exists "{{ this.name }}__index_on_{{ column }}" on {{ this.schema }}.{{ this }} ("{{ column }}")
 
 {% endmacro %}
